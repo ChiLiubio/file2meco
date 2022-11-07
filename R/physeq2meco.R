954cf13a6293829635948a273e6985cac4188d55
ChiLiubio <liuchi0426@126.com>
2022-11-07 20:15:56 +0800

    add sequence conversion for phyloseq

--- R/physeq2meco.R
+++ R/physeq2meco.R
@@ -14,6 +14,13 @@ meco2phyloseq <- function(dataset){
 	sample_table_trans <- dataset$sample_table
 	tax_table_trans <- dataset$tax_table
 	phylo_tree_trans <- dataset$phylo_tree
+	seq_trans <- dataset$rep_fasta
+	if(!is.null(seq_trans)){
+		if(!inherits(seq_trans, "DNAStringSet")){
+			message("Skip rep_fasta conversion as it is not DNAStringSet class!")
+			seq_trans <- NULL
+		}
+	}
 	# OTU table for phyloseq
 	otu_table_trans <- phyloseq::otu_table(otu_table_trans, taxa_are_rows = TRUE)
 	sampledata <- phyloseq::sample_data(sample_table_trans)
@@ -22,7 +29,7 @@ meco2phyloseq <- function(dataset){
 	if(!is.null(tax_table_trans)){
 		tax_table_trans <- phyloseq::tax_table(as.matrix(tax_table_trans))
 	}
-	physeq <- phyloseq::phyloseq(otu_table_trans, tax_table_trans, sampledata, phylo_tree_trans)
+	physeq <- phyloseq::phyloseq(otu_table_trans, tax_table_trans, sampledata, phylo_tree_trans, seq_trans)
 	physeq
 }
 
@@ -49,7 +56,9 @@ phyloseq2meco <- function(physeq, ...){
 	tax_table_trans <- as.data.frame(physeq@tax_table@.Data, check.names = FALSE, stringsAsFactors = FALSE)
 	tax_table_trans %<>% tidy_taxonomy
 	phylo_tree_trans <- physeq@phy_tree
-	
-	dataset <- microtable$new(sample_table = sample_table_trans, otu_table = otu_table_trans, tax_table = tax_table_trans, phylo_tree = phylo_tree_trans, ...)
+	seq_trans <- physeq@refseq
+
+	dataset <- microtable$new(sample_table = sample_table_trans, otu_table = otu_table_trans, 
+		tax_table = tax_table_trans, phylo_tree = phylo_tree_trans, rep_fasta = seq_trans, ...)
 	dataset
 }
