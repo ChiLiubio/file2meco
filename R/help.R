2486c40e240b7ce473fab59949990e11f4e0fdd6
ChiLiubio <liuchi0426@126.com>
2023-04-23 14:45:54 +0800

    add overall help document for file2meco

---
+++ R/help.R
@@ -0,0 +1,29 @@
+#' @title
+#' Introduction to file2meco package (\href{https://github.com/ChiLiubio/file2meco}{https://github.com/ChiLiubio/file2meco})
+#' 
+#' @description
+#' For the detailed tutorial on the file2meco package, please follow the tutorial link in the github repository
+#' (\href{https://github.com/ChiLiubio/file2meco}{https://github.com/ChiLiubio/file2meco})
+#' 
+#' Please open the help document by using \code{help} function or by clicking the following links collected:\cr
+#' \code{\link{qiime1meco}}\cr
+#' \code{\link{qiime2meco}}\cr
+#' \code{\link{humann2meco}}\cr
+#' \code{\link{mpa2meco}}\cr
+#' \code{\link{ncyc2meco}}\cr
+#' \code{\link{phyloseq2meco}}\cr
+#' \code{\link{meco2phyloseq}}\cr
+#' 
+#' To report bugs or discuss questions, please use Github Issues (\href{https://github.com/ChiLiubio/file2meco/issues}{https://github.com/ChiLiubio/file2meco/issues}).
+#' Before creating a new issue, 
+#' please read the guideline (\href{https://chiliubio.github.io/microeco_tutorial/notes.html#github-issues}{https://chiliubio.github.io/microeco_tutorial/notes.html#github-issues}).
+#' 
+#' To cite file2meco package in publications, please run the following command to get the reference: \code{citation("file2meco")}\cr
+#' 
+#' Reference:\cr
+#' Liu, C., Li, X., Mansoldo, F.R.P., An, J., Kou, Y., Zhang, X., Wang, J., Zeng, J., Vermelho, A.B., Yao, M., 2022. 
+#' Microbial habitat specificity largely affects microbial co-occurrence patterns and functional profiles in wetland soils. Geoderma 418, 115866.
+#' 
+#' @keywords Description
+#' @name file2meco
+NULL
